dceafa4752e7f48d23348c536d9b85caf8ad4eb0
Alex Bar <wgge82@gmail.com>
2026-04-10 11:28:52 +0300

    добавил Draw в абстрактный класс и рисование Arc

--- CNCSimulator/Geomerty/BaseGeometry.cs
+++ CNCSimulator/Geomerty/BaseGeometry.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CNCSimulator.Geomerty
 {
-    class BaseGeometry
+    abstract class BaseGeometry
     {
         internal Point point;
+
+        public  abstract void Draw(Graphics g);
+     
     }
 
 
--- CNCSimulator/Geomerty/Circle.cs
+++ CNCSimulator/Geomerty/Circle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace CNCSimulator.Geomerty
         private Int16 directRotation;
         private Point start;
         private Point end;
+        private PointF center;
 
         public Circle(Point _start, Point _end, Int16 _direct, double _radius)
         {
@@ -19,6 +21,9 @@ namespace CNCSimulator.Geomerty
             End = _end;
             directRotation = _direct;
             radius = _radius;
+            var c = GetCenter();
+            center = new PointF((float)c.x, (float)c.y);
+
         }
 
         private Point Start
@@ -66,6 +71,29 @@ namespace CNCSimulator.Geomerty
             double dy = End.y - End.x;
             return (Math.Sqrt(dx * dx + dy * dy), new Point(dx, dy));
 
+        }
+
+        public override void Draw(System.Drawing.Graphics g)
+        {
+            if (center == PointF.Empty) return;
+            float startAngle = (float)(Math.Atan2(Start.y - center.Y, Start.x - center.X) * 180 / Math.PI);
+            float endAngle = (float)(Math.Atan2(End.y - center.Y, End.x - center.X) * 180 / Math.PI);
+            float sweepAngle = endAngle - startAngle;
+
+            if (directRotation == 2) // По часовой (G02)
+            {
+                if (sweepAngle < 0) sweepAngle += 360;
+            }
+            else // Против часовой (G03)
+            {
+                if (sweepAngle > 0) sweepAngle -= 360;
+            }
+            float rectX = (float)(center.X - radius);
+            float rectY = (float)(center.Y - radius);
+            float rectSize = (float)(radius * 2);
+            RectangleF rect = new RectangleF(rectX, rectY, rectSize, rectSize);
+            Pen pen = new Pen(Color.Red, 2.0f);
+            g.DrawArc(pen, rect, startAngle, sweepAngle);
         }
     }
 }
